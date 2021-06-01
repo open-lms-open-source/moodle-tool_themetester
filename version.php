de86916a630063d714d14b720cf55f095a68ecfb
Fabian Batioja <fabian.batioja@openlms.net>
2021-06-01 22:14:04 +0000

    INT-17116: Bump versions for 3.10

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020061100; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2020061500; // Requires this Moodle version.
-$plugin->release   = '3.9.3';
+$plugin->version   = 2021060100; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2020110900; // Requires this Moodle version.
+$plugin->release   = '3.10.3';
 $plugin->component = 'tool_themetester'; // Full name of the plugin (used for diagnostics).
 
 $plugin->maturity  = MATURITY_STABLE; // This version's maturity level.

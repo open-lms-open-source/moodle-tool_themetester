940d35c5182c7e2454091210649b1842baf368b2
Fabian Batioja <fabian.batioja@openlms.net>
2021-11-26 16:25:24 +0000

    INT-17365: 3.11.3 Bump version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021060100; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021112600; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700; // Requires this Moodle version.
-$plugin->release   = '3.11.2';
+$plugin->release   = '3.11.3';
 $plugin->component = 'tool_themetester'; // Full name of the plugin (used for diagnostics).
 
 $plugin->maturity  = MATURITY_STABLE; // This version's maturity level.

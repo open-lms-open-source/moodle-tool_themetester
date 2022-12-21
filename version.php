0945ac46a228e3bff5cd6e59f48b90a3a5422432
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2022-12-21 23:09:52 +0000

    INT-18550: 4.0 Version bump

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022101200; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2021051700; // Requires this Moodle version.
-$plugin->release   = '3.11.6';
+$plugin->version   = 2022122100; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2022041900; // Requires this Moodle version.
+$plugin->release   = '4.0.5';
 $plugin->component = 'tool_themetester'; // Full name of the plugin (used for diagnostics).
 
 $plugin->maturity  = MATURITY_STABLE; // This version's maturity level.
